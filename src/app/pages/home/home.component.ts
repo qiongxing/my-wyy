f80563d36e75d6ed51c35a0f1e5a9209b6ecf88d
qiongxing <qiongxing1412@qq.com>
2019-11-26 00:07:51 +0800

    组件轮播完成，

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
 import { Banner } from 'src/app/types/common.model';
+import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,21 @@ import { Banner } from 'src/app/types/common.model';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  banners:Banner[];
+  banners: Banner[];
+  carouselIndex = 0;
+  @ViewChild(NzCarouselComponent, { static: true }) private nzCarousel: NzCarouselComponent;
   constructor(private homeSer: HomeService) {
     this.homeSer.getBanners().subscribe(banners => {
-      this.banners=banners;
+      this.banners = banners;
     });
   }
 
   ngOnInit() {
   }
-
+  onBeforeChange({ to }) {
+    this.carouselIndex = to;
+  }
+  onChangeSlide(type: 'pre' | 'next') {
+    this.nzCarousel[type]();
+  }
 }
