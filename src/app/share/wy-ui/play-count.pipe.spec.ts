89b3fcc0e0e72f5b0d3b54ec40cfe5f87b4fbfae
穹星 <qiongxing1412@qq.com>
2020-01-20 11:09:30 +0800

    新增计算关注数管道测试

--- src/app/share/wy-ui/play-count.pipe.spec.ts
+++ src/app/share/wy-ui/play-count.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { PlayCountPipe } from './play-count.pipe';
+import { TestBed, inject } from '@angular/core/testing';
 
-describe('PlayCountPipe', () => {
+fdescribe('PlayCountPipe', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PlayCountPipe]
+    })
+  })
   it('create an instance', () => {
     const pipe = new PlayCountPipe();
     expect(pipe).toBeTruthy();
   });
-});
+  it('number>10000显示万', inject([PlayCountPipe], (countPipe: PlayCountPipe) => {
+    expect(countPipe.transform(20000)).toEqual('2万')
+  }));
+  it('传入边界值', () => {
+    const pipe = new PlayCountPipe();
+    expect(pipe.transform(2 ** 53 - 1)).toBeTruthy();
+  })
+}); 
