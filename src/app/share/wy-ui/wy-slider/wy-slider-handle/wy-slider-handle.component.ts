d4690f0e2ae52fc72d2d53c3c44a765c596434c5
qiongxing <qiongxing1412@qq.com>
2020-06-09 16:34:51 +0800

    滑块组件（二）

--- src/app/share/wy-ui/wy-slider/wy-slider-handle/wy-slider-handle.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-handle/wy-slider-handle.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { WySliderStyle } from '../wy-slider-type';
 
 @Component({
   selector: 'wy-slider-handle',
-  template: `<div class='wy-slider-handle'></div>`,
+  template: `<div class='wy-slider-handle' [ngStyle]="style"></div>`,
   styles: []
 })
-export class WySliderHandleComponent implements OnInit {
-
+export class WySliderHandleComponent implements OnInit, OnChanges {
+  @Input() wyVertical = false;
+  @Input() wyOffset: number;
+  style: WySliderStyle = {};
   constructor() { }
 
   ngOnInit() {
   }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['wyOffset']) {
+      this.style[this.wyVertical ? 'bottom' : 'left'] = this.wyOffset + '%';
+    }
+  }
 }
--- src/app/share/wy-ui/wy-slider/wy-slider-track/wy-slider-track.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-track/wy-slider-track.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
+import { WySliderStyle } from '../wy-slider-type';
 
 @Component({
   selector: 'wy-slider-track',
-  template: `<div class='wy-slider-track'></div>`,
-  styles: []
+  template: `<div class='wy-slider-track' [ngStyle]="style"></div>`,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WySliderTrackComponent implements OnInit {
+export class WySliderTrackComponent implements OnInit, OnChanges {
 
+  @Input() wyVertical = false;
+  @Input() wyLength: number;
+  style: WySliderStyle = {};
   constructor() { }
 
   ngOnInit() {
   }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['wyLength']) {
+      if (this.wyVertical) {
+        this.style.height = this.wyLength + '%';
+        this.style.width = null;
+        this.style.left = null;
+      } else {
+        this.style.width = this.wyLength + '%';
+        this.style.height = null;
+        this.style.bottom = null;
+      }
+    }
+  }
 }
