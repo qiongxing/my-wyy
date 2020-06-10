8a0cb3580857e9fae8619ce1c23b63854ec28486
qiongxing <qiongxing1412@qq.com>
2020-06-10 17:48:25 +0800

    滑块组件（三）

--- src/app/share/wy-ui/wy-slider/wy-slider-type.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-type.ts
@@ -1,6 +1,19 @@
+import { Observer, Observable } from 'rxjs'
+
 export type WySliderStyle = {
     bottom?: string | null;
     height?: string | null;
     width?: string | null;
     left?: string | null;
+}
+export type WySliderMouseObserver = {
+    start: string;
+    move: string;
+    end: string;
+    filter: (e: Event) => boolean;
+    pluckKey: string[];
+    startPlucked$?: Observable<number>;
+    moveResolved$?: Observable<number>;
+    end$?: Observable<Event>;
+
 }
--- src/app/share/wy-ui/wy-slider/wy-slider.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, ElementRef, ViewChild, Inject, Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { WySliderMouseObserver } from './wy-slider-type';
+import { fromEvent, Observable, merge } from 'rxjs';
+import { filter, tap, pluck, map, distinctUntilChanged, takeUntil } from 'rxjs/internal/operators';
+import { sliderEvent } from './wy-slider.helper';
 
 @Component({
   selector: 'wy-slider',
@@ -8,10 +13,56 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WySliderComponent implements OnInit {
+  private sliderDom: HTMLDivElement;
+  @Input() wyVertical = false;
+  @ViewChild('wySlider', { static: true }) private wySlider: ElementRef;
 
-  constructor() { }
+  private dragStart$: Observable<number>;
+  private dragMove$: Observable<number>;
+  private dragEnd$: Observable<Event>;
+  constructor(@Inject(DOCUMENT) private doc: Document) { }
 
   ngOnInit() {
+    this.sliderDom = this.wySlider.nativeElement;
+    this.createDraggingObservables();
+  }
+  createDraggingObservables() {
+    const orientField = this.wyVertical ? 'pageY' : 'pageX';
+    const mouse: WySliderMouseObserver = {
+      start: 'mousedown',
+      move: 'mousemove',
+      end: 'mouseup',
+      filter: (e: MouseEvent) => e instanceof MouseEvent,
+      pluckKey: [orientField]
+    };
+    const touch: WySliderMouseObserver = {
+      start: 'touchstart',
+      move: 'touchmove',
+      end: 'touchend',
+      filter: (e: TouchEvent) => e instanceof TouchEvent,
+      pluckKey: ['touches', '0', orientField]
+    };
+    [mouse, touch].forEach(source => {
+      const { start, move, end, filter: filterFunc, pluckKey } = source;
+      source.startPlucked$ = fromEvent(this.sliderDom, start).pipe(
+        filter(filterFunc),
+        tap(sliderEvent),
+        pluck(...pluckKey),
+        map((position: number) => this.findClosestValue(position))
+      )
+      source.end$ = fromEvent(this.doc, end);
+      source.moveResolved$ = fromEvent(this.sliderDom, move).pipe(
+        filter(filterFunc),
+        tap(sliderEvent),
+        pluck(...pluckKey),
+        distinctUntilChanged(),
+        map((position: number) => this.findClosestValue(position)),
+        takeUntil(source.end$)
+      )
+    })
+    this.dragStart$ = merge(mouse.startPlucked$, touch.startPlucked$);
+    this.dragMove$ = merge(mouse.moveResolved$, touch.moveResolved$);
+    this.dragEnd$ = merge(mouse.end$, touch.end$);
   }
 
 }
