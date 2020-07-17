eaa1682c5c2f6af68d0f5433073016427a24270d
qiongxing <qiongxing1412@qq.com>
2020-07-17 13:28:54 +0800

    window对象使用

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,15 +1,24 @@
-import { NgModule, InjectionToken } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, InjectionToken, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
-export const API_CONFIG=new InjectionToken('ApiConfigToken')
+export const API_CONFIG = new InjectionToken('ApiConfigToken')
+export const WINDOW = new InjectionToken('WindowToken')
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers:[
-    {provide:API_CONFIG,useValue:'http://localhost:3000/'}
+  providers: [
+    { provide: API_CONFIG, useValue: 'http://localhost:3000/' },
+    {
+      provide: WINDOW,
+      useFactory: (platformId): Window | Object => {
+        return isPlatformBrowser(platformId) ? window : {}
+      },
+      deps: [PLATFORM_ID]
+    },
+
   ]
 })
 export class ServicesModule { }
